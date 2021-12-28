bcbba722391f223366ccad67b8190a2c49c604b5
Alejandro Lindarte Castro <alejo.lindarte@outlook.com>
2021-12-28 15:12:52 -0500

    Jackson dependency and Comerssia WSDL

---
+++ initial/src/main/java/com/example/springboot/XMLDeserialize.java
@@ -0,0 +1,20 @@
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import java.com.example.springboot.dto.Test;
+
+
+public class XMLDeserialize {
+    public void whenJavaGotFromXmlFile_thenCorrect() throws IOException {
+        File file = new File("src/main/resources/wsdl/test.xml");
+        XmlMapper xmlMapper = new XmlMapper();
+        String xml = inputStreamToString(new FileInputStream(file));
+        Test value = xmlMapper.readValue(xml, Test.class);
+        
+    }
+}
+
