894b72846e6c27e8d603db4a229539784e614115
Alejandro Lindarte Castro <alejo.lindarte@outlook.com>
2022-01-11 10:28:38 -0500

    Update

--- initial/src/main/java/com/example/springboot/dto/CustomResponse.java
+++ initial/src/main/java/com/example/springboot/dto/CustomResponse.java
@@ -13,24 +13,96 @@ public class CustomResponse
     private String e_ld_loc;
     private String e_ld_qty_oh;
 
-    public CustomResponse(String ld_domain, String ld_site) {
-        this.ld_domain = ld_domain;
-        this.ld_site = ld_site;
+    public CustomResponse(String pt_domain, String pt_part, String pt_article, String pt_desc1, String pt_desc2, String pt_status, String e_ld_domain, String e_ld_site, String e_ld_loc, String e_ld_qty_oh) {
+        this.pt_domain = pt_domain;
+        this.pt_part = pt_part;
+        this.pt_article = pt_article;
+        this.pt_desc1 = pt_desc1;
+        this.pt_desc2 = pt_desc2;
+        this.pt_status = pt_status;
+        this.e_ld_domain = e_ld_domain;
+        this.e_ld_site = e_ld_site;
+        this.e_ld_loc = e_ld_loc;
+        this.e_ld_qty_oh = e_ld_qty_oh;
     }
 
-    public String getLd_domain() {
-        return ld_domain;
+    public String getPt_domain() {
+        return pt_domain;
     }
 
-    public void setLd_domain(String ld_domain) {
-        this.ld_domain = ld_domain;
+    public void setPt_domain(String pt_domain) {
+        this.pt_domain = pt_domain;
     }
 
-    public String getLd_site() {
-        return ld_site;
+    public String getPt_part() {
+        return pt_part;
     }
 
-    public void setLd_site(String ld_site) {
-        this.ld_site = ld_site;
+    public void setPt_part(String pt_part) {
+        this.pt_part = pt_part;
+    }
+
+    public String getPt_article() {
+        return pt_article;
+    }
+
+    public void setPt_article(String pt_article) {
+        this.pt_article = pt_article;
+    }
+
+    public String getPt_desc1() {
+        return pt_desc1;
+    }
+
+    public void setPt_desc1(String pt_desc1) {
+        this.pt_desc1 = pt_desc1;
+    }
+
+    public String getPt_desc2() {
+        return pt_desc2;
+    }
+
+    public void setPt_desc2(String pt_desc2) {
+        this.pt_desc2 = pt_desc2;
+    }
+
+    public String getPt_status() {
+        return pt_status;
+    }
+
+    public void setPt_status(String pt_status) {
+        this.pt_status = pt_status;
+    }
+
+    public String getE_ld_domain() {
+        return e_ld_domain;
+    }
+
+    public void setE_ld_domain(String e_ld_domain) {
+        this.e_ld_domain = e_ld_domain;
+    }
+
+    public String getE_ld_site() {
+        return e_ld_site;
+    }
+
+    public void setE_ld_site(String e_ld_site) {
+        this.e_ld_site = e_ld_site;
+    }
+
+    public String getE_ld_loc() {
+        return e_ld_loc;
+    }
+
+    public void setE_ld_loc(String e_ld_loc) {
+        this.e_ld_loc = e_ld_loc;
+    }
+
+    public String getE_ld_qty_oh() {
+        return e_ld_qty_oh;
+    }
+
+    public void setE_ld_qty_oh(String e_ld_qty_oh) {
+        this.e_ld_qty_oh = e_ld_qty_oh;
     }
 }
--- initial/src/main/java/com/example/springboot/repository/QADRepository.java
+++ initial/src/main/java/com/example/springboot/repository/QADRepository.java
@@ -15,19 +15,19 @@ public interface QADRepository extends CrudRepository<Object, Long> {
     /**
      * Esto es
      */
-    @Query("select new com.example.springboot.dto.CustomResponse (p.pt_domain, p.pt_part, p.pt_article
-    p.pt_desc1
-    p.pt_desc2
-    p.pt_status
-    e.ld_domain
-    e.ld_site
-    e.ld_loc
-    e.ld_qty_oh) from Ld_det e, Pt_mstr p 
-    WHERE ld_det.ld_domain = global_domain AND
-    ld_det.ld_site        = alm_destino  AND
-    ld_det.ld_loc         = "+"TRA001"+" AND /*Ubicacion Transito*/
-    ld_det.ld_qty_oh > 0
-    AND pt_mstr.pt_domain  = global_domain  AND
-        pt_mstr.pt_part        = ld_det.ld_part ")
+    @Query("select new com.example.springboot.dto.CustomResponse (p.pt_domain, p.pt_part, p.pt_article"+
+            "    p.pt_desc1"+
+            "    p.pt_desc2"+
+            "    p.pt_status"+
+            "    e.ld_domain"+
+            "    e.ld_site"+
+            "    e.ld_loc"+
+            "    e.ld_qty_oh) from Ld_det e, Pt_mstr p "+
+            "    WHERE ld_det.ld_domain = global_domain AND"+
+            "    ld_det.ld_site        = alm_destino  AND"+
+            "    ld_det.ld_loc         = "+"TRA001"+" AND /*Ubicacion Transito*/"+
+            "    ld_det.ld_qty_oh > 0"+
+            "    AND pt_mstr.pt_domain  = global_domain  AND"+
+            "        pt_mstr.pt_part        = ld_det.ld_part")
     List<CustomResponse> getInfoFromQAD();
 }
