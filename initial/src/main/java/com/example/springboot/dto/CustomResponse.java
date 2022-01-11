42ebef4290e0db0759ca5a6205378d14dbf5ca68
Alejandro Lindarte Castro <alejo.lindarte@outlook.com>
2022-01-11 08:12:02 -0500

    Update

--- initial/src/main/java/com/example/springboot/dto/CustomResponse.java
+++ initial/src/main/java/com/example/springboot/dto/CustomResponse.java
@@ -2,8 +2,16 @@ package com.example.springboot.dto;
 
 public class CustomResponse
 {
-    private String ld_domain;
-    private String ld_site;
+    private String pt_domain;
+    private String pt_part;
+    private String pt_article;
+    private String pt_desc1;
+    private String pt_desc2;
+    private String pt_status;
+    private String e_ld_domain;
+    private String e_ld_site;
+    private String e_ld_loc;
+    private String e_ld_qty_oh;
 
     public CustomResponse(String ld_domain, String ld_site) {
         this.ld_domain = ld_domain;
--- initial/src/main/java/com/example/springboot/utils/Constantes.java
+++ initial/src/main/java/com/example/springboot/utils/Constantes.java
@@ -5,4 +5,10 @@ public class Constantes {
 
     public static String USUARIO = "624154454F2912704B025C5D0F4253016E2203ECDF907B";
     public static String PASSWORD = "624154454F2912704B025C5D0F4253016E6429CED4AF3E3C59";
+
+    public static String DOMAIN = "cerracol";
+
+    public static String ENVIADA = "Enviada";
+    public static String RECIBIDA = "Recibida";
+    
 }
